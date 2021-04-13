d40becd699b64b3a059f3ccffc0ad34d8588e577
Emerson Munoz <munoz.e@pucp.edu.pe>
2021-04-12 20:26:47 -0500

    plataformas mas plataforma repository

--- src/main/java/edu/pucp/gtics/lab1_gtics_20211/entity/Plataformas.java
+++ src/main/java/edu/pucp/gtics/lab1_gtics_20211/entity/Plataformas.java
@@ -1,7 +1,39 @@
 package edu.pucp.gtics.lab1_gtics_20211.entity;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "plataformas")
 public class Plataformas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idplataforma;
+    private String nombre;
+    private String descripcion;
+
+    public Integer getIdplataforma() {
+        return idplataforma;
+    }
+
+    public void setIdplataforma(Integer idplataforma) {
+        this.idplataforma = idplataforma;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
     /** Completar*/
 
--- src/main/java/edu/pucp/gtics/lab1_gtics_20211/repository/PlataformasRepository.java
+++ src/main/java/edu/pucp/gtics/lab1_gtics_20211/repository/PlataformasRepository.java
@@ -1,5 +1,8 @@
 package edu.pucp.gtics.lab1_gtics_20211.repository;
 
 
-public interface PlataformasRepository  {
+import edu.pucp.gtics.lab1_gtics_20211.entity.Plataformas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlataformasRepository extends JpaRepository<Plataformas,Integer> {
 }
