aa43fc0f73187ea6a2ee7b6ca25e604454b346f5
Rubi Polanco <a20183238@pucp.edu.pe>
2021-04-12 19:58:34 -0500

    juegos controller, lista de juegos

--- src/main/java/edu/pucp/gtics/lab1_gtics_20211/controller/JuegosController.java
+++ src/main/java/edu/pucp/gtics/lab1_gtics_20211/controller/JuegosController.java
@@ -1,18 +1,43 @@
 package edu.pucp.gtics.lab1_gtics_20211.controller;
 
+import edu.pucp.gtics.lab1_gtics_20211.repository.JuegosRepository;
+import edu.pucp.gtics.lab1_gtics_20211.repository.PlataformasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/juegos")
 public class JuegosController {
 
-    public String listaJuegos (){
-;
+    @Autowired
+    JuegosRepository juegosRepository;
+
+    @Autowired
+    PlataformasRepository plataformasRepository;
+
+    @GetMapping("/lista")
+    public String listaJuegos (Model model){
+
+        model.addAttribute("juegosLista",juegosRepository.findAll());
+
+        return "juegos/lista";
     }
 
-    public String editarJuegos(){
+    @GetMapping("/editar")
+    public String editarJuegos(Model model){
 
+        model.addAttribute("plataformasLista",plataformasRepository.findAll());
+
+        return "juegos/editar";
     }
 
+    @PostMapping("/guardar")
     public String guardarJuegos(){
-
+        return "redirect:/juegos/lista";
     }
 
 
